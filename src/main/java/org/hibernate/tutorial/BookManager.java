762b3fe7ea8eb6150da6313ee306736d6c4cc048
Tiago Luis <tmmluis@gmail.com>
2015-09-25 13:42:03 +0100

    Added an option to list all books inserted in the DB

--- src/main/java/org/hibernate/tutorial/BookManager.java
+++ src/main/java/org/hibernate/tutorial/BookManager.java
@@ -1,5 +1,7 @@
 package org.hibernate.tutorial;
 
+import java.util.List;
+
 import org.hibernate.Session;
 
 import org.hibernate.tutorial.simple_books.Book;
@@ -7,28 +9,44 @@ import org.hibernate.tutorial.util.HibernateUtil;
 
 public class BookManager {
 
-    public static void main(String[] args) {
-        BookManager mgr = new BookManager();
+	public static void main(String[] args) {
+		BookManager mgr = new BookManager();
 
-        if (args[0].equals("store")) {
-            mgr.createAndStoreBook("Thinking in Java", "Bruce Eckel", "English");
-        }
+		if (args[0].equals("store")) {
+			mgr.createAndStoreBook("Thinking in Java", "Bruce Eckel", "English");
+		} else if (args[0].equals("list")) {
+			List<Book> books = mgr.listBooks();
+			for (Book theBook : books) {
+				System.out.println("Book: " + theBook.getTitle() + " Author: " + theBook.getAuthor() + " Language: "
+						+ theBook.getLanguage());
+			}
+		}
 
-        HibernateUtil.getSessionFactory().close();
-    }
+		HibernateUtil.getSessionFactory().close();
+	}
 
-    private void createAndStoreBook(String title, String author, String language) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+	private List<Book> listBooks() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
-
-        Book theBook = new Book();
-        theBook.setTitle(title);
-        theBook.setAuthor(author);
-        theBook.setLanguage(language);
         
-        session.save(theBook);
-
+        // Need to revisit this expression to ensure type safety.
+        List<Book> result = session.createQuery("from Book").list();
         session.getTransaction().commit();
-    }
+        return result;
+	}
+
+	private void createAndStoreBook(String title, String author, String language) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		Book theBook = new Book();
+		theBook.setTitle(title);
+		theBook.setAuthor(author);
+		theBook.setLanguage(language);
+
+		session.save(theBook);
+
+		session.getTransaction().commit();
+	}
 
 }
